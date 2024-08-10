d774f6cd9dac3b1a78636dc5d454f6cd598a9947
ShossKe <shosuke.uenishi@outlook.jp>
2024-08-10 17:38:21 +0900

    Update base.py

--- base.py
+++ base.py
@@ -3,21 +3,21 @@ import sys
 
 data = input("type file name: ")
 data = open(data,"r",encoding="utf-8")
-data = data.readline()
 
 
-if data[0].isdigit():
-    s = eval(data)
-    print(s)             
-else: 
-    data = data.split("(")
-    bun = data[1].split(")")
-    bun = bun[0]
-    bun = bun.replace('"','')
-    if data[0] == "prints":
-        print(bun,"\n")
-    elif data[0] == "print":
-        print(bun)
+for line in data.readlines():
+    if line[0].isdigit():
+        s = eval(data)
+        print(s)             
+    elif line[0] == "p": 
+        line = line.split("(")
+        bun = line[1].split(")")
+        bun = bun[0]
+        bun = bun.replace('"','')
+        if line[0] == "prints":
+            print(bun,"\n")
+        elif line[0] == "print":
+            print(bun)
 
 
 
